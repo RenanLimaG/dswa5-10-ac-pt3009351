8acfd74dc87c9b904836c96a13fb9770fde4936e
RenanLimaG <renanlimag10@gmail.com>
2022-05-11 19:46:33 -0300

    Mongodb

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var http = require('http');
 var app = require('./config/express')();
-http.createServer(app).listen(app.get('port'), function(){
-	console.log('Express Server escutando na porta ' + app.get('port'));
+const url = 'mongodb+srv://dswa5:dswa5@cluster0.3fuxs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+require('./config/database.js')(url);
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express Server escutando na porta ' + app.get('port'));
 });
